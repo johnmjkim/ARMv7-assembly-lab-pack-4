9bb7f02fd2b8f180a2aa525a3eceac4a77dc766f
Min Jae Kim <johnmjkim1216@gmail.com>
2022-05-19 13:33:24 +1000

    task 1 completed

--- week-11/src/main.c
+++ week-11/src/main.c
@@ -1,4 +1,20 @@
+#include <stdio.h>
+
+int addThreeNumbers( int a, int b, int c )
+{
+    int sum =0;
+    sum += a;
+    sum += b;
+    sum += c;
+    return sum;
+}
+
 int main(void)
 {
+  int a = 3;
+  int b = 4;
+  int c = 7;
+  int sum = addThreeNumbers(a, b, c);
+
   return 0;
 }
